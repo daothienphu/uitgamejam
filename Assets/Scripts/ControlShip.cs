a9e1c4fd8a3231964d24124aacb5a944d4467ad1
Dao Thien Phu <daothienphu1429@gmail.com>
2022-11-26 10:17:29 +0700

    Init

---
+++ Assets/Scripts/ControlShip.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class ControlShip : MonoBehaviour
+{
+    Vector2 currentMovement;
+    void Start()
+    {
+        currentMovement = Vector2.zero;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Move(
+){
+    
+    }
+}
